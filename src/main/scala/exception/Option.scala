131d36b4037fad134301edb4a0186ec459f91fba
dengguoqing <dengguoqing93@163.com>
2018-11-22 13:33:40 +0800

    状态学习

--- src/main/scala/exception/Option.scala
+++ src/main/scala/exception/Option.scala
@@ -1,5 +1,7 @@
 package exception
 
+import charpter3.datastructures.Cons
+
 /**
   * $DESCRIPTION
   *
@@ -42,6 +44,35 @@ sealed trait Option[+A] {
     }
   }
 
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+    }
+  }
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a flatMap (aa => b map (bb => f(aa, bb)))
+  }
+
+  def sequence_1[A](a: List[Option[A]]): Option[List[A]] = {
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
+  }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
+    }
+  }
+
+  def traverse_1[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a.foldRight[Option[List[B]]](Some(Nil))((h, t) => map2(f(h), t)(_ :: _))
+  }
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] = {
+    traverse(a)(x => x)
+  }
 
 }
 
@@ -57,5 +88,32 @@ object Option {
 
   def variance(xs: Seq[Double]): Option[Double] = {
     mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a flatMap (aa => b map (bb => f(aa, bb)))
+  }
+
+  def map3[A, B, C, D](a: Option[A], b: Option[B], c: Option[C])(f: (A, B, C) => D): Option[D] = {
+    a flatMap (aa => b flatMap (bb => c map (cc => f(aa, bb, cc))))
+  }
+
+  def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double = {
+    1.0
+  }
+
+  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double] = {
+    val optAge: Option[Int] = Try(age.toInt)
+    val optTickets: Option[Int] = Try(numberOfSpeedingTickets.toInt)
+    map2(optAge, optTickets)(insuranceRateQuote)
+  }
+
+  def Try[A](a: => A): Option[A] = {
+    try Some(a)
+    catch {
+      case Exception => None
+    }
   }
 }
